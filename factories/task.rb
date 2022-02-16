6b60f0592bd76a3c8df474684f99abec3697663a
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2022-02-16 11:51:10 +0900

    Update task.rb

--- factories/task.rb
+++ factories/task.rb
@@ -1,30 +1,30 @@
 FactoryBot.define do
   factory :task1, class: Task  do
-    title { 'タスク1' }
+    title { 'タスクのタイトル1' }
     description { 'タスクの内容1' }
     status { 'todo' }
     deadline { '2020-01-01' }
   end
   factory :task2, class: Task  do
-    title { 'タスク2' }
+    title { 'タスクのタイトル12' }
     description { 'タスクの内容2' }
     status { 'doing' }
     deadline { '2021-01-01' }
   end
   factory :task3, class: Task  do
-    title { 'タスク3' }
+    title { 'タスクのタイトル3' }
     description { 'タスクの内容3' }
     status { 'done' }
     deadline { '2023-01-01' }
   end
   factory :task4, class: Task  do
-    title { 'タスク4' }
-    description { 'タスクの内容4' }
+    title { 'タスクのタイトル4' }
+    description { 'タスクの内容34' }
     status { 'todo' }
     deadline { '2024-01-01' }
   end
   factory :task5, class: Task  do
-    title { 'タスク5' }
+    title { 'タスクのタイトル5' }
     description { 'タスクの内容5' }
     status { 'doing' }
     deadline { '2025-01-01' }
