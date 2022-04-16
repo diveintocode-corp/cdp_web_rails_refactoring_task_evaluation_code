5debdde14ac24749922162426bbc9706b38a69f9
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2022-04-16 19:40:29 +0900

    Update my_formatter.rb

--- my_formatter.rb
+++ my_formatter.rb
@@ -10,7 +10,9 @@ class MyFormatter
   end
 
   def stop(notification)
-    if @pass_or_fail
+    if notification.examples.count == 0
+      @output << @output << "## Evaluation could not be performed. Please check for syntax errors in your code.\n### Common syntax errors\n- Typos in code.\n- The `end` is missing."
+    elsif @pass_or_fail
       @output << "✅ The result of the automated assessment is a pass. If you have completed all the refactorings presented in the assignment requirements, please submit your assignment from the DIVER assignment submission screen."
     else
       @output << "❌ The result of the automated evaluation is failing. Please fix your code to pass all tests locally."
